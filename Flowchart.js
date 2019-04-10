194a298907f813fee7fac7ce9402df570fed92fc
Jin Kwon <jinseokkwon@hotmail.com>
2019-04-10 15:05:27 -0400

    0.1

--- Flowchart.js
+++ Flowchart.js
@@ -1,16 +1,24 @@
-var username ='jin';
+var username =client_id;
 var amount =1000;
 var message ='Hello, world';
 var sender=username;
-var today=Date();
+var today=Date().replace(/\s/g, '');
+var filename=username+today.substring(0,20);
+
+function save(){
+    //I have not understood perfectly yet.
+    fs.writeFile(`data/${filename}`,message,'utf8', function(err){});
+}
 
  function submit(info1,info2,info3,info4){
-    
+if(logedin){
+    save(info1,info2,infor3,info4)
+    //this place is send message
+}else[
+    alert("Please, log in to send a meesage to your streamer.");
+] 
 }
-function save(info1,info2,info3,info4){
 
-
-}
 
 
 
