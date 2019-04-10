280b6aee37b0ab489c6a5780c6a1899a86cb5416
Jinseok Kwon <jinseokkwon@hotmail.com>
2019-04-10 08:38:49 -0400

    0.1
    no updates

---
+++ Flowchart.js
@@ -0,0 +1,22 @@
+var username ='jin';
+var amount =1000;
+var message ='Hello, world';
+var sender=username;
+var today=Date();
+
+ function submit(info1,info2,info3,info4){
+    
+}
+function save(info1,info2,info3,info4){
+
+
+}
+
+
+
+if(message.length<=1){
+    alert("Please, leave a message");
+    }else{
+    submit(username,amount,message,sender);
+    save(username,amount,message.sender);
+}
